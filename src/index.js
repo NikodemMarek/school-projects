3d995f7994227f2a1254892b1e740af7ed3927fa
Nikodem <nikodemmarek11@gmail.com>
2021-11-03 22:28:22 +0100

    feat: added elements

--- src/index.js
+++ src/index.js
@@ -1,77 +1,70 @@
 import drawBoard from './draw/drawBoard.js'
 import Board from './data/board.js'
 
-// Dummy code.
 const boardContainer = document.getElementsByClassName('board')[0]
 const boardContainerSize = {
     x: 250,
     y: 250
 }
 const boardSize = {
-    x: 5,
-    y: 5
+    x: 9,
+    y: 9
 }
-const elementProperties = {
+
+let elementProperties = {
     0: {
-        class: 'e0'
+        class: 'covered'
     },
     1: {
-        class: 'e1',
-        content: `<div style='background-color: blue; width: 100%; height: 100%'></div>`,
-        eventListeners: [
-            {
-                event: 'click',
-                perform: (element, event, elementData) => alert('he clicked me')
-            }
-        ]
+        class: 'uncovered',
+        content: `<span style='color: blue;'>1</span>`
     },
     2: {
-        class: 'e0',
-        eventListeners: [
-            {
-                event: 'contextmenu',
-                perform: (element, event, elementData) => {
-                    element.style.backgroundColor = elementData.clicked? 'white': 'black'
-                    elementData.clicked = !elementData.clicked
-                }
-            }
-        ],
-        additionalData: {
-            clicked: false
-        }
+        class: 'uncovered',
+        content: `<span style='color: green;'>2</span>`
     },
     3: {
-        class: 'e1',
-        content: `<div style='background-color: blue; width: 100%; height: 100%'></div>`,
-        eventListeners: [
-            {
-                event: 'mousemove',
-                perform: (element, event, elementData) => {
-                    elementData.position.x = event.clientX
-                    elementData.position.y = event.clientY
-                }
-            }
-        ],
-        additionalData: {
-            position: {
-                x: 0,
-                y: 0
-            }
-        }
+        class: 'uncovered',
+        content: `<span style='color: red;'>3</span>`
+    },
+    4: {
+        class: 'uncovered',
+        content: `<span style='color: darkblue;'>4</span>`
+    },
+    5: {
+        class: 'uncovered',
+        content: `<span style='color: darkred;'>5</span>`
+    },
+    6: {
+        class: 'uncovered',
+        content: `<span style='color: lightseagreen;'>6</span>`
+    },
+    7: {
+        class: 'uncovered',
+        content: `<span style='color: black;'>7</span>`
+    },
+    8: {
+        class: 'uncovered',
+        content: `<span style='color: gray;'>8</span>`
+    },
+    9: {
+        class: 'flag',
+        content: `<span style='color: tomato;'>|</span>`
+    },
+    10: {
+        class: 'bomb',
+        content: `<span style='color: black;'>o</span>`
+    },
+    11: {
+        class: 'bombExploded',
+        content: `<span style='color: black;'>o</span>`
     }
 }
+
 const spacing = {
     x: 1,
     y: 1
 }
-const boardData = [
-    [ 0, 0, 1, 0, 0 ],
-    [ 2, 3, 2, 0, 1 ],
-    [ 0, 0, 3, 0, 0 ],
-    [ 1, 2, 0, 3, 0 ],
-    [ 1, 0, 0, 1, 2 ]
-]
-
 const board = new Board(
     boardContainer,
     boardContainerSize,
@@ -79,9 +72,21 @@ const board = new Board(
     elementProperties,
     {
         spacing: spacing,
-        boardData: boardData,
-        boardColor: 'red'
+        boardColor: 'black'
     }
 )
+
+board.board.boardData[1][1] = 1
+board.board.boardData[2][1] = 2
+board.board.boardData[3][1] = 3
+board.board.boardData[4][1] = 4
+board.board.boardData[5][1] = 5
+board.board.boardData[6][1] = 6
+board.board.boardData[7][1] = 7
+board.board.boardData[8][1] = 8
+
+board.board.boardData[2][3] = 9
+board.board.boardData[3][3] = 10
+board.board.boardData[4][3] = 11
 
 drawBoard(board)
